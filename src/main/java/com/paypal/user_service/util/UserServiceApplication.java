7a176324ebac526b87a77595cd8e946634f3f5f9
Garima chaudhary <garimachaudhary1205@gmail.com>
2025-08-26 21:59:08 +0530

    change

--- src/main/java/com/paypal/user_service/util/UserServiceApplication.java
+++ src/main/java/com/paypal/user_service/util/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.paypal.user_service;
+package com.paypal.user_service.util;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+
+        SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
