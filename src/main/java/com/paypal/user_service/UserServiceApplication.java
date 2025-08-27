db520c6998b520763300d5ea4a833ed095fe1e14
Garima chaudhary <garimachaudhary1205@gmail.com>
2025-08-27 16:24:41 +0530

    change in code

--- src/main/java/com/paypal/user_service/UserServiceApplication.java
+++ src/main/java/com/paypal/user_service/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.paypal.user_service.util;
+package com.paypal.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
